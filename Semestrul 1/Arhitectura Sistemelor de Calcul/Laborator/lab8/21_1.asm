2d73b2363bbf83bb904eb617c9df83598c14e24d
Cosmin-Daniel <147927162+cocsmin@users.noreply.github.com>
2024-09-05 18:28:18 +0300

    Add files via upload

---
+++ Semestrul 1/Arhitectura Sistemelor de Calcul/Laborator/lab8/21_1.asm
@@ -0,0 +1,66 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+extern printf
+extern scanf
+import printf msvcrt.dll
+import scanf msvcrt.dll
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ;Sa se citeasca de la tastatura doua numere a si b de tip word. Sa se afiseze in baza 16 numarul c de tip dword pentru care partea low este suma celor doua numere, iar partea high este diferenta celor doua numere. Exemplu:
+    ;a = 574, b = 136
+    ;c = 01B602C6h
+    ; ...
+    a dw 0
+    b dw 0
+    c dd 0
+    citire_a db "a= ", 0
+    citire_b db "b= ", 0
+    afisare db "c= ", 0
+    format db "%d", 0
+    format2 db "%x", 0
+
+; our code starts here
+segment code use32 class=code
+    start:
+        push dword citire_a
+        call [printf]
+        add esp, 4*1
+        push dword a
+        push dword format
+        call [scanf]
+        add esp, 4*2
+        
+        push dword citire_b
+        call [printf]
+        add esp, 4*1
+        push dword b
+        push dword format
+        call [scanf]
+        add esp, 4*2
+        
+        mov AX, [a]
+        add AX, [b] ; AX = a + b
+        
+        mov DX, [a]
+        sub DX, [b] ; DX = a - b
+        
+        mov [c], AX
+        mov [c+2], DX
+        
+        push dword [c+2]
+        push dword [c]
+        push dword format2
+        call [printf]
+        add esp, 4*3
+        
+        
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ Semestrul 1/Arhitectura Sistemelor de Calcul/Laborator/lab8/21_2.asm
@@ -0,0 +1,75 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit, fopen, fclose, fprintf, fread              ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll
+import fopen msvcrt.dll
+import fclose msvcrt.dll
+import fprintf msvcrt.dll
+import fread msvcrt.dll   ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+    text db "3 mere 2 pere 3215g de faina", 0
+    lungime equ $-text-1
+    nume db "rezultat.txt", 0
+    acces db "w", 0
+    id dd -1
+    rezultat times lungime+1 db 0
+; our code starts here
+segment code use32 class=code
+    start:
+        ;Se dau un nume de fisier si un text (definite in segmentul de date). Textul contine litere mici, cifre si spatii. Sa se inlocuiasca toate cifrele de pe pozitii impare cu caracterul ‘X’. Sa se creeze un fisier cu numele dat si sa se scrie textul obtinut in fisier.
+        ; ...
+        cld
+        mov ESI, text
+        mov EDI, rezultat
+        mov ECX, lungime
+        mov BH, 0
+        repeta:
+            lodsb
+            test BH, 1
+            jz next
+            cmp AL, '0'
+            jb next
+            cmp AL, '9'
+            ja next
+            mov AL, 'X'
+            
+            next:
+            stosb
+            add BH, 1
+        loop repeta
+        
+        mov AL, 0
+        stosb ; ca sa pot pune in fisier
+        
+        
+        push dword acces
+        push dword nume
+        call [fopen]
+        add ESP, 4*2
+        
+        mov [id], EAX
+        cmp EAX, 0
+        je final
+        
+        push dword rezultat
+        push dword [id]
+        call [fprintf]
+        add ESP, 4*2
+        
+        push dword [id]
+        call [fclose]
+        add esp, 4*1
+        
+        
+        final:
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
