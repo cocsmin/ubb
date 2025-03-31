6832279d1bb4742e35243213fdd2df91eac07c38
Cosmin-Daniel <147927162+cocsmin@users.noreply.github.com>
2025-03-31 15:24:20 +0300

    Add files via upload

---
+++ Semestrul 4/SGBD/lab1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Tema1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Conectare = new System.Windows.Forms.Button();
+            this.gridBAUTURI = new System.Windows.Forms.DataGridView();
+            this.gridMENIU = new System.Windows.Forms.DataGridView();
+            this.Adaugare = new System.Windows.Forms.Button();
+            this.Stergere = new System.Windows.Forms.Button();
+            this.Actualizare = new System.Windows.Forms.Button();
+            this.ldenumire = new System.Windows.Forms.Label();
+            this.lcantitate = new System.Windows.Forms.Label();
+            this.lpret = new System.Windows.Forms.Label();
+            this.DENUMIRE = new System.Windows.Forms.TextBox();
+            this.CANTITATE = new System.Windows.Forms.TextBox();
+            this.PRET = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridBAUTURI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMENIU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Conectare
+            // 
+            this.Conectare.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Conectare.Location = new System.Drawing.Point(712, 12);
+            this.Conectare.Name = "Conectare";
+            this.Conectare.Size = new System.Drawing.Size(463, 87);
+            this.Conectare.TabIndex = 0;
+            this.Conectare.Text = "Conectare";
+            this.Conectare.UseVisualStyleBackColor = false;
+            this.Conectare.Click += new System.EventHandler(this.Conectare_Click);
+            // 
+            // gridBAUTURI
+            // 
+            this.gridBAUTURI.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gridBAUTURI.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridBAUTURI.GridColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gridBAUTURI.Location = new System.Drawing.Point(995, 115);
+            this.gridBAUTURI.Name = "gridBAUTURI";
+            this.gridBAUTURI.RowHeadersWidth = 82;
+            this.gridBAUTURI.RowTemplate.Height = 33;
+            this.gridBAUTURI.Size = new System.Drawing.Size(905, 518);
+            this.gridBAUTURI.TabIndex = 2;
+            // 
+            // gridMENIU
+            // 
+            this.gridMENIU.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gridMENIU.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridMENIU.GridColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.gridMENIU.Location = new System.Drawing.Point(21, 115);
+            this.gridMENIU.Name = "gridMENIU";
+            this.gridMENIU.RowHeadersWidth = 82;
+            this.gridMENIU.RowTemplate.Height = 33;
+            this.gridMENIU.Size = new System.Drawing.Size(913, 518);
+            this.gridMENIU.TabIndex = 3;
+            // 
+            // Adaugare
+            // 
+            this.Adaugare.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Adaugare.Location = new System.Drawing.Point(102, 686);
+            this.Adaugare.Name = "Adaugare";
+            this.Adaugare.Size = new System.Drawing.Size(463, 108);
+            this.Adaugare.TabIndex = 4;
+            this.Adaugare.Text = "Adaugare";
+            this.Adaugare.UseVisualStyleBackColor = false;
+            this.Adaugare.Click += new System.EventHandler(this.Adaugare_Click);
+            // 
+            // Stergere
+            // 
+            this.Stergere.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Stergere.Location = new System.Drawing.Point(712, 686);
+            this.Stergere.Name = "Stergere";
+            this.Stergere.Size = new System.Drawing.Size(463, 108);
+            this.Stergere.TabIndex = 5;
+            this.Stergere.Text = "Stergere";
+            this.Stergere.UseVisualStyleBackColor = false;
+            this.Stergere.Click += new System.EventHandler(this.Stergere_Click);
+            // 
+            // Actualizare
+            // 
+            this.Actualizare.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Actualizare.Location = new System.Drawing.Point(1307, 686);
+            this.Actualizare.Name = "Actualizare";
+            this.Actualizare.Size = new System.Drawing.Size(463, 108);
+            this.Actualizare.TabIndex = 8;
+            this.Actualizare.Text = "Actualizare";
+            this.Actualizare.UseVisualStyleBackColor = false;
+            this.Actualizare.Click += new System.EventHandler(this.Actualizare_Click);
+            // 
+            // ldenumire
+            // 
+            this.ldenumire.AutoSize = true;
+            this.ldenumire.Location = new System.Drawing.Point(97, 845);
+            this.ldenumire.Name = "ldenumire";
+            this.ldenumire.Size = new System.Drawing.Size(126, 25);
+            this.ldenumire.TabIndex = 9;
+            this.ldenumire.Text = "Numele:";
+            // 
+            // lcantitate
+            // 
+            this.lcantitate.AutoSize = true;
+            this.lcantitate.Location = new System.Drawing.Point(1302, 845);
+            this.lcantitate.Name = "lcantitate";
+            this.lcantitate.Size = new System.Drawing.Size(121, 25);
+            this.lcantitate.TabIndex = 11;
+            this.lcantitate.Text = "Cantitatea:";
+            // 
+            // lpret
+            // 
+            this.lpret.AutoSize = true;
+            this.lpret.Location = new System.Drawing.Point(707, 845);
+            this.lpret.Name = "lpret";
+            this.lpret.Size = new System.Drawing.Size(131, 25);
+            this.lpret.TabIndex = 12;
+            this.lpret.Text = "Pretul:";
+            // 
+            // DENUMIRE
+            // 
+            this.DENUMIRE.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.DENUMIRE.ForeColor = System.Drawing.SystemColors.Window;
+            this.DENUMIRE.Location = new System.Drawing.Point(239, 842);
+            this.DENUMIRE.Name = "denumire";
+            this.DENUMIRE.Size = new System.Drawing.Size(326, 31);
+            this.DENUMIRE.TabIndex = 15;
+            // 
+            // CANTITATE
+            // 
+            this.CANTITATE.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.CANTITATE.ForeColor = System.Drawing.SystemColors.Window;
+            // Am schimbat locația pentru a fi lângă eticheta "Cantitatea:"
+            this.CANTITATE.Location = new System.Drawing.Point(1451, 845);
+            this.CANTITATE.Name = "cantitate";
+            this.CANTITATE.Size = new System.Drawing.Size(321, 31);
+            this.CANTITATE.TabIndex = 14;
+            // 
+            // PRET
+            // 
+            this.PRET.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.PRET.ForeColor = System.Drawing.SystemColors.Window;
+            // Am schimbat locația pentru a fi lângă eticheta "Pretul:"
+            this.PRET.Location = new System.Drawing.Point(854, 842);
+            this.PRET.Name = "pret";
+            this.PRET.Size = new System.Drawing.Size(319, 31);
+            this.PRET.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(1958, 945);
+            this.Controls.Add(this.DENUMIRE);
+            this.Controls.Add(this.CANTITATE);
+            this.Controls.Add(this.PRET);
+            this.Controls.Add(this.lpret);
+            this.Controls.Add(this.lcantitate);
+            this.Controls.Add(this.ldenumire);
+            this.Controls.Add(this.Actualizare);
+            this.Controls.Add(this.Stergere);
+            this.Controls.Add(this.Adaugare);
+            this.Controls.Add(this.gridMENIU);
+            this.Controls.Add(this.gridBAUTURI);
+            this.Controls.Add(this.Conectare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridBAUTURI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridMENIU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Conectare;
+        private System.Windows.Forms.DataGridView gridMENIU;
+        private System.Windows.Forms.DataGridView gridBAUTURI;
+        private System.Windows.Forms.Button Adaugare;
+        private System.Windows.Forms.Button Stergere;
+        private System.Windows.Forms.Button Actualizare;
+        private System.Windows.Forms.Label ldenumire;
+        private System.Windows.Forms.Label lcantitate;
+        private System.Windows.Forms.Label lpret;
+        private System.Windows.Forms.TextBox DENUMIRE;
+        private System.Windows.Forms.TextBox CANTITATE;
+        private System.Windows.Forms.TextBox PRET;
+    }
+}
---
+++ Semestrul 4/SGBD/lab1/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Tema1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source=192.168.1.148,1433;Initial Catalog=GLOVO;User ID=sa;Password=12345OHdf%e;");
+        SqlDataAdapter daO = new SqlDataAdapter();
+        SqlDataAdapter daT = new SqlDataAdapter();
+        DataSet dsO = new DataSet();
+        DataSet dsT = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+            gridMENIU.SelectionChanged += gridMENIU_SelectionChanged;
+            Stergere.Click += Stergere_Click;
+            Actualizare.Click += Actualizare_Click;
+            Adaugare.Click += Adaugare_Click;
+        }
+
+        private void Conectare_Click(object sender, EventArgs e)
+        {
+            daT.SelectCommand = new SqlCommand("SELECT * FROM MENIU", cs);
+            dsT.Clear();
+            daT.Fill(dsT);
+            gridMENIU.DataSource = dsT.Tables[0];
+        }
+
+        private void gridMENIU_SelectionChanged(object sender, EventArgs e)
+        {
+            if (gridMENIU.SelectedRows.Count > 0)
+            {
+                int id_MENIU = Convert.ToInt32(gridMENIU.SelectedRows[0].Cells["ID_MENIU"].Value);
+                LoadBAUTURI(id_MENIU);
+            }
+        }
+
+        private void LoadBAUTURI(int id_MENIU)
+        {
+            try
+            {
+                string query = "SELECT * FROM BAUTURI WHERE ID_MENIU = @id_MENIU";
+                daO.SelectCommand = new SqlCommand(query, cs);
+                daO.SelectCommand.Parameters.Clear();
+                daO.SelectCommand.Parameters.AddWithValue("@id_MENIU", id_MENIU);
+
+                dsO.Clear();
+                gridBAUTURI.DataSource = null;
+                daO.Fill(dsO);
+                gridBAUTURI.DataSource = dsO.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+
+        private void Adaugare_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!ValidareInput()) return;
+
+                int id_MENIU = Convert.ToInt32(gridMENIU.SelectedRows[0].Cells["id_MENIU"].Value);
+
+                daO.InsertCommand = new SqlCommand("INSERT INTO BAUTURI (ID_MENIU, DENUMIRE, CANTITATE, PRET) VALUES (@t, @n, @s, @l)", cs);
+                daO.InsertCommand.Parameters.Add("@t", SqlDbType.Int).Value = id_MENIU;
+                daO.InsertCommand.Parameters.Add("@n", SqlDbType.VarChar).Value = DENUMIRE.Text;
+                daO.InsertCommand.Parameters.Add("@s", SqlDbType.Int).Value = int.Parse(CANTITATE.Text);
+                daO.InsertCommand.Parameters.Add("@l", SqlDbType.Int).Value = int.Parse(PRET.Text);
+
+                cs.Open();
+                daO.InsertCommand.ExecuteNonQuery();
+                cs.Close();
+
+                MessageBox.Show("Adaugarea s-a realizat cu succes!");
+                LoadBAUTURI(id_MENIU);
+                GolireTextBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+                cs.Close();
+            }
+        }
+
+        private void Stergere_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (gridBAUTURI.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Selecteaza o bautura pentru a o sterge!");
+                    return;
+                }
+
+                int id_BAUTURA = Convert.ToInt32(dsO.Tables[0].Rows[gridBAUTURI.CurrentCell.RowIndex][0]);
+
+                daO.DeleteCommand = new SqlCommand("DELETE FROM BAUTURI WHERE ID_BAUTURA = @id_BAUTURA", cs);
+                daO.DeleteCommand.Parameters.Add("@id_BAUTURA", SqlDbType.Int).Value = id_BAUTURA;
+
+                cs.Open();
+                daO.DeleteCommand.ExecuteNonQuery();
+                cs.Close();
+
+                MessageBox.Show("Stergerea s-a realizat cu succes!");
+                int id_MENIU = Convert.ToInt32(gridMENIU.SelectedRows[0].Cells["ID_MENIU"].Value);
+                LoadBAUTURI(id_MENIU);
+                GolireTextBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+                cs.Close();
+            }
+        }
+
+        private void Actualizare_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (gridBAUTURI.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Selecteaza o bautura pentru a o actualiza!");
+                    return;
+                }
+
+                if (!ValidareInput()) return;
+
+                int id_BAUTURA = Convert.ToInt32(dsO.Tables[0].Rows[gridBAUTURI.CurrentCell.RowIndex][0]);
+
+                daO.UpdateCommand = new SqlCommand("UPDATE BAUTURI SET DENUMIRE = @n, CANTITATE = @s, PRET = @l WHERE ID_BAUTURA = @id", cs);
+                daO.UpdateCommand.Parameters.Add("@n", SqlDbType.VarChar).Value = DENUMIRE.Text;
+                daO.UpdateCommand.Parameters.Add("@s", SqlDbType.Int).Value = int.Parse(CANTITATE.Text);
+                daO.UpdateCommand.Parameters.Add("@l", SqlDbType.Int).Value = int.Parse(PRET.Text);
+                daO.UpdateCommand.Parameters.Add("@id", SqlDbType.Int).Value = id_BAUTURA;
+
+                cs.Open();
+                daO.UpdateCommand.ExecuteNonQuery();
+                cs.Close();
+
+                MessageBox.Show("Actualizarea s-a realizat cu succes!");
+                int id_MENIU = Convert.ToInt32(gridMENIU.SelectedRows[0].Cells["ID_MENIU"].Value);
+                LoadBAUTURI(id_MENIU);
+                GolireTextBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+                cs.Close();
+            }
+        }
+
+        private bool ValidareInput()
+        {
+            if (string.IsNullOrWhiteSpace(DENUMIRE.Text) || string.IsNullOrWhiteSpace(CANTITATE.Text) || string.IsNullOrWhiteSpace(PRET.Text))
+            {
+                MessageBox.Show("Toate campurile trebuie completate!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (!Regex.IsMatch(DENUMIRE.Text, @"^[a-zA-Z\s]+$"))
+            {
+                MessageBox.Show("Numele bauturii trebuie sa contina doar litere!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (!int.TryParse(CANTITATE.Text, out int nr) || !int.TryParse(PRET.Text, out int nr2) || nr <= 0 || nr2 <= 0)
+            {
+                MessageBox.Show("Pretul bauturii sau cantitatea acesteia trebuie sa fie un numar pozitiv!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (ExistaBAUTURA(DENUMIRE.Text))
+            {
+                MessageBox.Show("Bautura exista deja in baza de date!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ExistaBAUTURA(string numeBAUTURA)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM BAUTURI WHERE DENUMIRE = @nume", cs);
+            cmd.Parameters.AddWithValue("@nume", numeBAUTURA);
+            cs.Open();
+            int count = (int)cmd.ExecuteScalar();
+            cs.Close();
+            return count > 0;
+        }
+
+        public void GolireTextBox()
+        {
+            DENUMIRE.Text = "";
+            CANTITATE.Text = "";
+            PRET.Text = "";
+        }
+    }
+}
